9b83e1bba09b56e38d92a80187fd349ab9002f0a
yangshiyu <151250181@smail.nju.edu.cn>
2018-07-01 00:17:04 +0800

    最初框架

--- simulation/param.py
+++ simulation/param.py
@@ -6,3 +6,30 @@ Parameter、OperatingTimes
 文件目标：
 定义过程参数
 """
+
+# 列出了可供参考的参数，请根据实际情况修改，将来可删 # TODO
+"""
+   public static  double fl = 0.1;
+   public static  double fup = 0.15;
+   public static  double fdown = 0.12;
+   public static  double ft = 0.12;
+   public static  double fad = 0.05;
+   public static  double ps = 0.5;
+   public static  double lamdan = 25;
+   public static  double lamdaq =0.25;
+   public static  double lamdac = 0.1;
+   public static  double a = 1;
+   public static  double p = 0.5;  //Z转移概率
+   public static  Strategy str;
+   
+   public static double beita = 0.6;    //fsch判断阈值
+   public static  int x = 0; //greedy算法选择 0在线 or 1离线
+   public static final int n = 1;	//greedy算法选择 0reward or 1fsch
+"""
+local_fail_rate = 0.0  # 本地调度故障率
+
+cloud_up_fail_r = 0.0  # 云服务器上传故障率
+cloud_down_fail_r = 0.0  # 云服务器下载故障率
+
+p = 0.5  # Z转移概率
+beta = 0.6  # fsch判断阈值
--- simulation/simulation.py
+++ simulation/simulation.py
@@ -6,3 +6,97 @@ Simulation、SimulationsInput、SimulationOut
 类目标：
 实现模拟过程
 """
+
+
+def simulation_start():
+    """
+    开始模拟
+    对应方法：SimulationsInput.simulationStart
+    """
+    pass
+
+
+def schedule(map, tasks, beta, n):
+    """
+    调度
+    对应方法：Simulation.DiaoDu
+    :param map: 状态值-<策略，回报值>映射
+    :param tasks: 任务列表
+    :param beta: fsch判断阈值
+    :param n: 
+    :type map: map<State,map<stategy,double>>
+    :type tasks: tasks[]
+    :type beta: double
+    :type n: int
+    """
+    pass
+
+
+def learning(map, state):
+    """
+    学习
+    对应方法：Simulation.learningA
+    :param map: 状态值-<策略，回报值>映射
+    :param state: 状态
+    :type map: map<State,map<stategy,double>>
+    :type state: struts.state
+    :return: 调度策略
+    :rtype: strategy
+    """
+    pass
+
+
+def environment_simulation(param):
+    """
+    模拟环境
+    对应方法：Simulation.enviroSimulation
+    :param param: 环境参数
+    :type param: param
+    :return: 环境
+    :rtype: environment
+    """
+    pass
+
+
+def calculate_reward(state, strategy, task, n):
+    """
+    选择动作
+    对应方法：Simulation.SelectAction
+    :param state: 状态
+    :param strategy: 策略
+    :param task: 任务
+    :type state: struts.state
+    :type task: task
+    :type strategy: strategy
+    :return: reword
+    :rtype: reword
+    """
+    pass
+
+
+def select_action(map, state, task):
+    """
+    选择Strategy
+    对应方法：Simulation.SelectAction
+    :param map: 状态值-<策略，回报值>映射
+    :param state: 状态
+    :param task: 任务
+    :type map: map<state,map<strategy,double>>
+    :type state: struts.state
+    :type task: task
+    :return: 策略
+    :rtype: strategy
+    """
+    pass
+
+
+def poisson_process(p_lamda):
+    """
+    模拟泊松过程
+    对应方法：Simulation.RandExp
+    :param p_lamda: 参数
+    :type p_lamda: double
+    :return: 随机密度
+    :rtype: double
+    """
+    pass
