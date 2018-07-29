636bddf2ace204e9b4bc4ddc3a6071e0d0e3cea5
lianyouCat <945926945@qq.com>
2018-07-29 14:31:00 +0800

    加入数据分组

--- main/train/shuju.py
+++ main/train/shuju.py
@@ -2,21 +2,17 @@
 import random
 import numpy as np
 
+la = 0.3
+exp = 10
+var = 3
 
-if __name__ == '__main__':
-    la = 0.3
-    N = 100000
+exp2 = 0.8
+var2 = 0.2
+
+
+def write_data(file, N):
     xs = [random.expovariate(la) for i in range(N)]
     ps = np.cumsum(xs)
-
-    exp = 10
-    var = 3
-
-    exp2 = 0.8
-    var2 = 0.2
-
-    txtName = "./data.txt"
-    f = open(txtName, "a+")
 
     for i in range(0, N):
         cd = random.normalvariate(exp, var)
@@ -24,12 +20,18 @@ if __name__ == '__main__':
             cd = 0.5
         rd = random.normalvariate(exp2, var2) * cd
         ls = cd * 2.5
-        f.write(str(ps[i]))
-        f.write(',')
-        f.write(str(cd))
-        f.write(',')
-        f.write(str(rd))
-        f.write(',')
-        f.write(str(ls))
-        f.write('\n')
-    f.close()
+        file.write(str(ps[i]))
+        file.write(',')
+        file.write(str(cd))
+        file.write(',')
+        file.write(str(rd))
+        file.write(',')
+        file.write(str(ls))
+        file.write('\n')
+
+
+if __name__ == '__main__':
+    with open('train_data.txt', mode='a+') as f:
+        write_data(f, 80 * 1000)  # 80组，每组1000个
+    with open('data.txt', mode='a+')as f:
+        write_data(f, 20 * 1000)
